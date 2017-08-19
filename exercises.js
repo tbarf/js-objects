3810c3788600c00d30b3ba81657f8defee0f525d
tbarf <30032737+tbarf@users.noreply.github.com>
2017-08-18 16:46:46 -1000

    seven done

--- exercises.js
+++ exercises.js
@@ -111,11 +111,23 @@ console.log(stockCar);
             - adding a property called `age` and set its value to be the third parameter.
 
    The function will return the first parameter.
-   Finally, invoke your new function while passing in the `plainPerson` object and additional agrument values, then store
+   Finally, invoke your new function while passing in the `plainPerson` object and additional argument values, then store
    the return value in a variable named `completePerson`. Use `console.log` three times to print the entire object, just
    the value at `name`, and just the value at `age`.
  */
+// Built a person
+var plainPerson = {};
+var plainPerson1 ={};
+var plainPerson2= {};
+function buildPerson(person,nameString,age){
+person.name = nameString;
+person.age = age;
+return person;
+}
 
+console.log(buildPerson(plainPerson, "tom", 21));
+console.log(buildPerson(plainPerson1, "Phil", 6));
+console.log(buildPerson(plainPerson2, "Steve", 50));
 
 /*
 7. Display values of objects that are inside an array
@@ -141,6 +153,69 @@ console.log(stockCar);
             =====
             ...
  */
+var arrayOfObjects = [
+  {
+    id: 0,
+    date: "Monday Jan 25 2015 2:01 PM",
+    total: "279.38"
+  },
+  {
+    id: 1,
+    date: "Monday Jan 27 2015 11:31 AM",
+    total: "79.80"
+  },
+  {
+    id: 2,
+    date: "Monday Feb 1 2015 7:56 AM",
+    total: "15.62"
+  },
+  {
+    id: 3,
+    date: "Monday Feb 1 2015 9:43 AM",
+    total: "19.83"
+  },
+  {
+    id: 4,
+    date: "Monday Feb 1 2015 11:08 PM",
+    total: "56.69"
+  },
+  {
+    id: 5,
+    date: "Monday Feb 13 2015 10:22 AM",
+    total: "137.92"
+  },
+  {
+    id: 6,
+    date: "Monday Feb 14 2015 6:54 PM",
+    total: "938.65"
+  },
+  {
+    id: 7,
+    date: "Monday Feb 14 2015 7:17 PM",
+    total: "43.77"
+  },
+  {
+    id: 8,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "28.54"
+  },
+  {
+    id: 9,
+    date: "Monday Feb 14 2015 7:18 PM",
+    total: "194.33"
+  }
+];
+
+// console.log((JSON.stringify(arrayOfObjects, null, 4)));
+function printOrders(orders){
+  for(var i = 0; i<orders.length; i++){
+   console.log("===");
+   console.log(orders[i].id);
+   console.log(orders[i].date);
+   console.log(orders[i].total);
+  }
+};
+console.log(printOrders(arrayOfObjects));
 
 
 /*
@@ -155,6 +230,13 @@ console.log(stockCar);
         Invoke your function and pass in your object, store the result to a variable named sumObjResult and use `console.log`
         to inspect your results.
 */
+// var sumObj = {
+//   a: 2,
+//   b: 3,
+//   result:
+// }
+// function objectAddition({};)
+
 
 
 /*
